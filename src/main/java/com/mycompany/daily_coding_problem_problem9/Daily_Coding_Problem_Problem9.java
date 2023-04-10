9fd821a1d4e7d7f074301458cdaa5e885f2f439c
Danielnaor <dnaorcode@gmail.com>
2023-04-10 12:05:55 -0500

    first commit

---
+++ src/main/java/com/mycompany/daily_coding_problem_problem9/Daily_Coding_Problem_Problem9.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.daily_coding_problem_problem9;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ *
+ * @author carmitnaor
+ */
+public class Daily_Coding_Problem_Problem9 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int[] list = {2, 4, 6, 2, 5};
+        System.out.println("for list: " + Arrays.toString(list) + " the largest sum of non-adjacent numbers is " + findTheLargestSum(list));
+        int[] list2 = {5, 1, 1, 5};
+        System.out.println("for list: " + Arrays.toString(list2) + " the largest sum of non-adjacent numbers is " + findTheLargestSum(list2));
+
+    }
+    
+    public static int findTheLargestSum(int[] list){
+        int len = list.length;
+        
+        if(len == 0){
+            return 0;
+        }
+        
+        if(len == 1){
+            return list[0];
+        }
+         
+        int[] sum = new int[len];
+        
+        sum[len-1] = list[len-1];
+        sum[len-2] = Math.max(list[len-1], list[len-2]);
+  
+        for(int i=len-3;i>=0;i--) {
+          sum[i] = Math.max(list[i] + sum[i+2], sum[i+1]);
+        }
+  
+        return sum[0];
+    }
+}
+
